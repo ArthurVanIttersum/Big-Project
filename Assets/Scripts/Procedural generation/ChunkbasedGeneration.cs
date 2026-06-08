31f586c4142725f6147762e37e58ec0c1acbc871
arthur <ajcvanittersum@gmail.com>
2026-06-08 12:26:29 +0200

    moved dictionary
    moved the dictionary from the chunk based generation script to the continuoue generation script. this makes it easier to expand the generation.

--- Assets/Scripts/Procedural generation/ChunkbasedGeneration.cs
+++ Assets/Scripts/Procedural generation/ChunkbasedGeneration.cs
@@ -12,36 +12,19 @@ public class ChunkBasedGeneration : MonoBehaviour
     public ChunkSettings chunkSettings;
 
 
-    //empty prefab
-    public GameObject prefabEmptyGameObject;
-
-    private Dictionary<int, GameObject> generatedChunks = new();
-
     
-    void GenerateChunk(int zPos)
+    void GenerateChunk(GameObject chunk)
     {
         print("Generating Chunk");
         if (generationSettings == null) return;//quick test
         if (chunkSettings == null) return;//quick test
-        if (generatedChunks.ContainsKey(zPos)) return;//quick test
         
         print("pastChecks");
-
-        //Create an empty gameobject representing a chunk.
-        //store it in the dictionary instead of storing individual objects.
-        GameObject chunk = Instantiate(prefabEmptyGameObject, Vector3.zero + Vector3.forward * zPos, Quaternion.identity, transform);
-        chunk.name = "Chunk " + zPos.ToString();
-        
-        print(chunk);
 
         //Add chunk to dictionary
         int chunkSize = chunkSettings.chunksize;
         
-        generatedChunks.Add(zPos, chunk);
-
-        print(generatedChunks.Count);
-        print(zPos);
-
+        
         //generate objects in chunk
         foreach (var item in generationSettings.objects)
         {
@@ -52,18 +35,16 @@ public class ChunkBasedGeneration : MonoBehaviour
                     0,
                     UnityEngine.Random.Range(-chunkSize / 2, chunkSize / 2)
                 );
-                Instantiate(item.prefab, randomOffset + Vector3.forward * zPos, Quaternion.identity, chunk.transform);
+                Instantiate(item.prefab, randomOffset + chunk.transform.position, Quaternion.identity, chunk.transform);
             }
         }
     }
 
 
-    void RemoveChunk(int zPos)
+    void RemoveChunk(GameObject removed)
     {
         print("Removing Chunk");
-        if (!generatedChunks.ContainsKey(zPos)) return;//quick test
-        print(zPos);
-        generatedChunks.Remove(zPos, out GameObject removed);
+        
         RemoveObjectHelper.RemoveObject(removed);
     }
 
