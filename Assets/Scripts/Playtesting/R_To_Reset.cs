67bf23e4c221a62be92ec47d1aa189723be93061
Danthelionxyz <daanijsseldijk@gmail.com>
2026-06-09 16:04:59 +0200

    Playtest and iteration
    Added smoothness to player and floor, tweaked values for playtest, camera  now follows properly, wip for reset script

---
+++ Assets/Scripts/Playtesting/R_To_Reset.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class R_To_Reset : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
