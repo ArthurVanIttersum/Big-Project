2d50a882386378efe96a319abc5b7f2ec1055de3
Danthelionxyz <daanijsseldijk@gmail.com>
2026-06-11 14:22:58 +0200

    Audio playtestable build

--- Assets/Scripts/Gameplay/PlayerSFXObserver.cs
+++ Assets/Scripts/Gameplay/PlayerSFXObserver.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 public class PlayerSFXObserver : PlayerObserver
 {
     //Add the proper sound effects to showcase when a player loses or wins
-    //[SerializeField] private List<AudioSource> audio = new List<AudioSource>();
+    [SerializeField] private List<AudioSource> audio = new List<AudioSource>();
 
     protected override void OnVSFX(int value)
     {
         if (value == 0)
         {
-            //audio[0].Play();
+            audio[0].Play();
             Debug.Log("SFX lose");
         }
 
         else
         {
-            //audio[1].Play();
+            audio[1].Play();
             Debug.Log("SFX win");
         }
     }
