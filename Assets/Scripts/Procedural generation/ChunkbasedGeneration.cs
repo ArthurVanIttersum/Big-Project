c0e39b8b24934060a1296d48660628f5c68dc987
arthur <ajcvanittersum@gmail.com>
2026-07-02 11:44:45 +0200

    changes from build
    these are the changes i made to get the build to work, and the changes that were made by the build.

--- Assets/Scripts/Procedural generation/ChunkbasedGeneration.cs
+++ Assets/Scripts/Procedural generation/ChunkbasedGeneration.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class ChunkBasedGeneration : MonoBehaviour
 {
